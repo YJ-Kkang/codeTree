bc762e143030201aa99517cf2db662783a9b018f
YJ-Kkang <kyujin995@gmail.com>
2024-12-11 09:26:22 +0900

    Title: 변수 출력하기 2, Time: 79ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241211/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        String b = "C";
+        String c = "!.....!";
+        System.out.println(b + c + a);
+    }
+}
