1fe396f6907b1348ebabe24f419e4d61ca39136c
YJ-Kkang <kyujin995@gmail.com>
2024-12-15 17:34:50 +0900

    Title: 입력받아 출력 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241215/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.print(num1);
+        System.out.print(num2);
+    }
+}
