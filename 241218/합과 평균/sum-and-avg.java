86f6c1f9dde7f235be65f282b54feb262678d845
YJ-Kkang <kyujin995@gmail.com>
2024-12-18 09:45:05 +0900

    Title: 합과 평균, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 241218/합과 평균/sum-and-avg.java
+++ 241218/합과 평균/sum-and-avg.java
@@ -9,3 +9,23 @@ public class Main {
         System.out.printf((a + b) + " %.1f", ((double)a + b) / 2);
     }
 }
+
+/*
+다른 방법:
+double avr = ((double) a + b) / 2;에서 double 생략하면
+전체 계산 값이 3이 된 다음에 double type(값: 3.0)으로 나오는 것이기에 원하는 것과 다른 결과가 나옴!
+*/
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         int sum = a + b;
+//         double avr = ((double) a + b) / 2;
+
+//         System.out.printf(sum + " %.1f", avr);
+//     }
+// }
